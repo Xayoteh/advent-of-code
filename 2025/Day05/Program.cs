fb65e566c28dcc22996dcc5cb1634ac3a9d5c983
xayoteh <javier.act080298@gmail.com>
2025-12-05 15:42:27 -0600

    2025 - day 5 - part 1

---
+++ 2025/Day05/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Solution to Advent of Code 2025 - Day 5
+https://adventofcode.com/2025/day/5
+*/
+
+const string INPUT_PATH = "../input.txt";
+
+var (ranges, ingredients) = ReadInput(INPUT_PATH);
+var freshIngredients = GetFreshIngredients(ranges, ingredients);
+
+Console.WriteLine("** Day 5 **");
+Console.WriteLine($"Part 1: {freshIngredients.Count()}");
+
+IEnumerable<long> GetFreshIngredients(List<(long start, long end)> ranges, List<long> ingredients)
+    => ingredients.Where(x => ranges.Any(r => x >= r.start && x <= r.end));
+
+(List<(long, long)>, List<long>) ReadInput(string path)
+{
+    using var file = new StreamReader(path);
+
+    List<(long, long)> ranges = [];
+    while (file.ReadLine() is string line && !string.IsNullOrWhiteSpace(line))
+    {
+        var parts = line.Split('-');
+        long rangeStart = long.Parse(parts[0]);
+        long rangeEnd = long.Parse(parts[1]);
+
+        ranges.Add((rangeStart, rangeEnd));
+    }
+
+    List<long> ingredients = [];
+    while (file.ReadLine() is string line)
+    {
+        ingredients.Add(long.Parse(line));
+    }
+
+    return (ranges, ingredients);
+}
