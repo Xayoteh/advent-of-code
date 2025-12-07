aecc9b3ae1daa3c90a0ae0a439750a7a7c928be5
xayoteh <javier.act080298@gmail.com>
2025-12-07 01:47:51 -0600

    2025 - day 6 - part 2

--- 2025/Day06/Program.cs
+++ 2025/Day06/Program.cs
@@ -5,46 +5,106 @@ https://adventofcode.com/2025/day/6
 
 const string INPUT_PATH = "../input.txt";
 
-List<Problem> problems = ReadInput(INPUT_PATH);
-long grandTotal = problems.Select(p => p.Solve()).Sum();
+string[][] problemsData = ReadInput(INPUT_PATH);
 
-Console.WriteLine("** Day 07 **");
-Console.WriteLine($"Part 1: {grandTotal}");
+Problem[] problems = ParseProblems(problemsData);
+long total = problems.Select(p => p.Solve()).Sum();
+
+Problem[] realProblems = ParseProblemsByColumn(problemsData);
+long realTotal = realProblems.Select(p => p.Solve()).Sum();
+
+Console.WriteLine("** Day 06 **");
+Console.WriteLine($"Part 1: {total}");
+Console.WriteLine($"Part 2: {realTotal}");
 
 
-List<Problem> ReadInput(string path)
+Problem[] ParseProblems(string[][] problemsData)
 {
-    List<List<int>> operands = [];
-    List<Problem> problems = [];
+    var problems = new Problem[problemsData[0].Length];
 
-    foreach (string line in File.ReadLines(path))
+    for(var c = 0; c < problemsData[0].Length; c++)
     {
-        var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var operands = new int[problemsData.Length - 1];
+        char operation = problemsData[^1][c][0];
 
-        for (var i = 0; i < parts.Length; i++)
-        {
-            if (int.TryParse(parts[i], out int operand))
-            {
-                if (operands.Count <= i)
-                    operands.Add([]);
-                operands[i].Add(operand);
-            }
-            else
-            {
-                problems.Add(new Problem(operands[i], parts[i][0]));
-            }
-        }
+        for(var r = 0; r < problemsData.Length - 1; r++)
+            operands[r] = int.Parse(problemsData[r][c]);
+
+
+        problems[c] = new Problem(operands, operation);
     }
 
     return problems;
 }
 
-record Problem(IEnumerable<int> operands, char operation)
+Problem[] ParseProblemsByColumn(string[][] problemsData)
 {
-    public long Solve() => operands.Aggregate(
-        operation is '+' ? 0L : 1L,
+    var problems = new Problem[problemsData[0].Length];
+
+    for(var c = 0; c < problemsData[0].Length; c++)
+    {
+        var operands = new int[problemsData[0][c].Length];
+        char operation = problemsData[^1][c][0];
+
+        for(var i = 0; i < problemsData[0][c].Length; i++)
+            for(var r = 0; r < problemsData.Length - 1; r++)
+                operands[i] = problemsData[r][c][i] == ' ' ? operands[i]
+                    : operands[i] * 10 + problemsData[r][c][i] - '0';
+
+        problems[c] = new Problem(operands, operation);
+    }
+
+    return problems;
+}
+
+// TODO: refactor this?
+string[][] ReadInput(string path)
+{
+    var lines = File.ReadAllLines(path);
+    List<int> columnsWidths = [];
+
+    string lastLine = lines.Last();
+    int start = 0;
+
+    while(lastLine.IndexOfAny(['+', '*'], start + 1) is int end && end != -1)
+    {
+        int width = end - start - 1;
+        columnsWidths.Add(width);
+        start = end;
+    }
+
+    columnsWidths.Add(lastLine.Length - start);
+
+    string[][] problemsData = new string[lines.Length][];
+
+    for(var i = 0; i < lines.Length - 1; i++)
+    {
+        var ss = new StringReader(lines[i]);
+
+        problemsData[i] = new string[columnsWidths.Count];
+
+        for(var j = 0; j < columnsWidths.Count; j++)
+        {
+            int width = columnsWidths[j];
+            var buffer = new char[width];
+
+            ss.ReadBlock(buffer, 0, width);
+            ss.Read();
+            problemsData[i][j] = new string(buffer);
+        }
+    }
+
+    problemsData[^1] = lastLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    return problemsData;
+}
+
+record Problem(IEnumerable<int> Operands, char Operation)
+{
+    public long Solve() => Operands.Aggregate(
+        Operation is '+' ? 0L : 1L,
         (result, operand) 
-            => operation is '+' ? result + operand : result * operand,
+            => Operation is '+' ? result + operand : result * operand,
         result => result
     );
 }
