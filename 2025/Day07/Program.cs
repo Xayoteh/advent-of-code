891113eda286d74857a5b374698244b48137062f
xayoteh <javier.act080298@gmail.com>
2025-12-07 21:08:32 -0600

    2025 - day 7 - part 2

--- 2025/Day07/Program.cs
+++ 2025/Day07/Program.cs
@@ -6,15 +6,47 @@ https://adventofcode.com/2025/day/7
 const string INPUT_PATH = "../input.txt";
 
 var (start, splittersRows) = ReadInput(INPUT_PATH);
-var (beams, splits) = Simulate(start, splittersRows);
+
+int splitsCount = CountSplits(start, splittersRows);
+long timelinesCount = CountTimelines(start, splittersRows);
 
 Console.WriteLine("** Day 7 **");
-Console.WriteLine($"Part 1: {splits}");
+Console.WriteLine($"Part 1: {splitsCount}");
+Console.WriteLine($"Part 2: {timelinesCount}");
+
+long CountTimelinesFromPoint(Point point, List<List<Point>> splittersRows, int rowIndex, Dictionary<(Point, int), long> memo)
+{
+    for (int r = rowIndex; r < splittersRows.Count; r++)
+    {
+        if (IsHit(point, splittersRows[r]))
+        {
+            var left = new Point(point.X + 1, point.Y + 1);
+            var right = new Point(point.X - 1, point.Y + 1);
+
+            return GetTimelinesFromPoint(left, splittersRows, r + 1, memo)
+                + GetTimelinesFromPoint(right, splittersRows, r + 1, memo);
+        }
+
+        point = new Point(point.X, point.Y + 1);
+    }
+
+    return 1;
+}
+
+long GetTimelinesFromPoint(Point point, List<List<Point>> splittersRows, int rowIndex, Dictionary<(Point, int), long> memo)
+    => memo.ContainsKey((point, rowIndex)) ? memo[(point, rowIndex)]
+        : memo[(point, rowIndex)] = CountTimelinesFromPoint(point, splittersRows, rowIndex, memo);
+
+long CountTimelines(Point start, List<List<Point>> splittersRows)
+{
+    Dictionary<(Point, int), long> memo = [];
+    return GetTimelinesFromPoint(start, splittersRows, 0, memo);
+}
 
 bool IsHit(Point beam, List<Point> splitters)
     => splitters.Any(s => s == beam);
 
-(Queue<Point>, int) Simulate(Point start, List<List<Point>> splittersRows)
+int CountSplits(Point start, List<List<Point>> splittersRows)
 {
     Queue<Point> beams = [];
     int splitCount = 0;
@@ -46,16 +78,16 @@ bool IsHit(Point beam, List<Point> splitters)
             beams.Enqueue(beam);
     }
 
-    return (beams, splitCount);
+    return splitCount;
 }
 
 
 (Point, List<List<Point>>) ReadInput(string path)
 {
     var lines = File.ReadAllLines(path);
-    var start = new Point(lines[0].IndexOf('S'), 1);
+    var start = new Point(lines[0].IndexOf('S'), 0);
 
-    List<List<Point>> splitters = [];
+    List<List<Point>> splitters = [[]];
 
     for (int i = 1; i < lines.Length; i++)
     {
